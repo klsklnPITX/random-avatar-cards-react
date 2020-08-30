ca1e39ce1fe3da530265358e3dfdbf4a14d43f30
klsklnPITX <67044656+klsklnPITX@users.noreply.github.com>
2020-08-30 21:18:39 +0200

    Added search functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from "react"
+import React, { Component } from "react"
 import CardList from "./CardList"
 import { avatar } from "./avatar"
 import SearchBox from "./SearchBox"
 
 
-const App = () => {
-    return (
-        <div className="tc">
-            <h1>Avatars</h1>
-            <SearchBox />
-            <CardList avatar={avatar} />
-        </div>
-    );
+
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            avatars: avatar,
+            searchField: ""
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value });
+    }
+
+    render() {
+        const filteredAvatars = this.state.avatars.filter(avatars => {
+            return avatars.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        })
+        return (
+            <div className="tc">
+                <h1>Avatars</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList avatar={filteredAvatars} />
+            </div>
+        );
+    }
 }
 
 export default App;
